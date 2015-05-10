2bb4f84f53f5e52b3de4c873a1afebbcf9d76dc1
Ziga Vukcevic <ziga@be-codified.com>
2015-05-10 13:09:07 +0200

    functionality added

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ angular.module('demo', []);
 
 angular.module('demo').controller('DemoController', function($scope) {
     $scope.tasks = [];
-    console.log($scope.tasks);
     
-    $scope.add = function(task) {
+    $scope.create = function(task) {
         $scope.tasks.push(task);
-        console.log('array: ' + $scope.tasks);
+    }
+
+    $scope.remove = function(index) {
+        $scope.tasks.splice(index);
     }
 });
