b96c83c1884b6f448bcba9ccd3ad6f26f1a51a07
Ziga Vukcevic <ziga@be-codified.com>
2015-05-11 09:32:02 +0200

    hiding/showing buttons moved to controller

--- js/main.js
+++ js/main.js
@@ -7,7 +7,21 @@ angular.module('demo').controller('DemoController', function($scope) {
         $scope.tasks.push(task);
     }
 
+    $scope.edit = function() {
+        $scope.showItem = false; 
+        $scope.showInput = true; 
+        $scope.showEditButton = false;
+        $scope.showCloseButton = true;
+    }
+
+    $scope.close = function() {
+        $scope.showItem = true; 
+        $scope.showInput = false; 
+        $scope.showEditButton = true;
+        $scope.showCloseButton = false;
+    }
+
     $scope.remove = function(index) {
-        $scope.tasks.splice(index);
+        $scope.tasks.splice(index, 1);
     }
 });
